c94a587b7c06666c345cd2e0c41c93bb2c5d8897
Nick Inman <nick@theoatmeal.com>
2019-01-25 01:44:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/" do
+    erb :index
+  end
+
+  get "/new" do
+    erb :create_puppy
+  end
+
+  post "/puppy" do
+    @puppy = Puppy.new(params.values[0], params.values[1], params.values[2])
+    erb :display_puppy
+  end
 end
